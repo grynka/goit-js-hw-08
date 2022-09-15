f2f3f41a6d127c805c4975e7e642c4182063af0a
grynka <ihor.kozhemyakin@gmail.com>
2022-09-16 00:27:43 +0300

    update video

---
+++ src/js/02-video.js
@@ -0,0 +1,38 @@
+import Vimeo from '@vimeo/player';
+import throttle from 'lodash.throttle';
+
+const iframe = document.querySelector('iframe');
+const player = new Vimeo(iframe);
+
+player
+  .setCurrentTime(localStorage.getItem('videoplayer-current-time'))
+  .then(function (seconds) {
+    // seconds = the actual time that the player seeked to
+  })
+  .catch(function (error) {
+    switch (error.name) {
+      case 'RangeError':
+        // the time was less than 0 or greater than the video’s duration
+        break;
+
+      default:
+        // some other error occurred
+        break;
+    }
+  });
+
+const onPlay = function (timeupdate) {
+    localStorage
+        .setItem("videoplayer-current-time",
+        JSON.stringify(timeupdate.seconds)
+    );
+};
+
+const throttleOnPlay = throttle(onPlay, 1000);
+
+player.on('timeupdate', throttleOnPlay);
+
+
+
+
+
