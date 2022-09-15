dd0e661fbb0af69340a8e1cc60514a32acd79df9
grynka <ihor.kozhemyakin@gmail.com>
2022-09-15 16:47:23 +0300

    progress part 2

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,1 +1,15 @@
+import "@vimeo/player";
 
+ const iframe = document.querySelector('iframe');
+ const player = new Vimeo.Player(iframe);
+
+const moment = 0;
+console.log(moment);
+     
+
+ const onPlay = function (data) {
+     moment = data.currentTime;
+
+ };
+
+ player.on('play', onPlay);
